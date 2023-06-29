6bdf9b2fc98dacfb8ff57c411e0742904f83e8f7
Manvendra Manu <rescue11080@gmail.com>
2023-06-29 21:12:49 +0300

    adding a class version with pyfftw

--- GPE/__init__.py
+++ GPE/__init__.py
@@ -1,4 +1,5 @@
 
 from .GPE_scalar_field import GPE_scalar_field
+from .GPE_scalar_field_w_pyfftw import GPE_scalar_field_w_pyfftw
 from .GPE_scalar_field_1d2c import GPE_scalar_field_1d2c
 from .ImEx import ImEx
